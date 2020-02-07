bf8e981d30d025b4b6e863594a33f1f6a63a3b88
Luis R. de la Vega Lopez <luisr.delavega@gmail.com>
2020-02-07 18:42:07 -0500

    Changing the project to Pokedex game

---
+++ precache-manifest.b17119ff167b6956103342cf5392cb15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7631538c5a94dbcf1ec492d825eb6f10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec0630bc72990b5be69c",
+    "url": "/static/css/main.bfd7b69e.chunk.css"
+  },
+  {
+    "revision": "306d93061bda7d810796",
+    "url": "/static/js/2.5e713fd1.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.5e713fd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec0630bc72990b5be69c",
+    "url": "/static/js/main.9fe91127.chunk.js"
+  },
+  {
+    "revision": "aaae6e212bbff495f23f",
+    "url": "/static/js/runtime-main.fff16644.js"
+  }
+]);
